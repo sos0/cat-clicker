a8dac073e90b44d3ea001bf2543378bce5d8fbf1
so <sosoi@ucdavis.edu>
2015-09-17 14:23:35 -0700

    adding functionality for five cats, moving around logic to make sure click handlers can be removed and added with the DOM as well

--- cat.js
+++ cat.js
@@ -1,15 +1,42 @@
 Cat = function(){
+	this.count	= 0;
+	this.name = "";
+	this.src = "";
+	this.DOM = null;
+
 	this.init = function(name, src){
 		this.name   = name;
 		this.src 	= src;
+		this.DOM	= $('#cat-template').clone().removeAttr('id');
 	}
 
-	this.count	= 0;
+	this.render = function(){
+		$(this.DOM).find('h3').text(this.name);
+		var catRef = this;
+		$(this.DOM).find('img').attr('src', this.src).click(function(){
+			catRef.countUp();
+			$(catRef.DOM).find('.count').text(catRef.count);
+		});
+		
+		$(this.DOM).appendTo('#five-cats');
+		$(this.DOM).show();
+	}
 
-	this.update = function(){
-		console.log('name:', this.name);
-		console.log('count:', this.count);
+	this.countUp = function(){
 		this.count++;
+	}
+
+	this.DOM	= null;
+	this.displayStatus = false;
+	this.toggle = function(){
+		if(this.displayStatus == false){
+			this.render();
+			this.displayStatus = true;
+		}
+		else{
+			$(this.DOM).remove();
+			this.displayStatus = false;
+		}
 	}
 }
 
@@ -17,26 +44,37 @@ Cats = {
 	catList: [],
 	initCats: function(){
 		var photoCat = new Cat();
-		photoCat.init('Real Kitty', 'kitten.png');
-		var illuCat  = new Cat();
-		illuCat.init('Drawing Kitty', 'kitten2.png');
+		photoCat.init('Kitten (Real)', 'assets/kitten.png');
 		this.catList.push(photoCat);
-		this.catList.push(illuCat);
+
+		var kittenCat  = new Cat();
+		kittenCat.init('Kitten', 'assets/kitten2.png');
+		this.catList.push(kittenCat);
+
+		var boxPrinceCat  = new Cat();
+		boxPrinceCat.init('Box Prince', 'assets/BoxPrince.png');
+		this.catList.push(boxPrinceCat);
+
+		var cakeCat  = new Cat();
+		cakeCat.init('Cake', 'assets/Cake_001.png');
+		this.catList.push(cakeCat);
+
+		var meMowCat  = new Cat();
+		meMowCat.init('MeMow', 'assets/MeMow.png');
+		this.catList.push(meMowCat);
 	}
 }
 
 Cats.initCats();
 
-$('#two-cats > .column').each(function(i){
+$('.list-container > .buttons').children().each(function(i){
 	var cat = Cats.catList[i];
-	var $newCat = $('#cat-template').clone();
-	$(this).append($newCat).removeAttr('id');
-
-	$newCat.find('h3').text(cat.name);
-	$newCat.find('img').attr('src', cat.src).click(function(){
-		cat.update();
-		$newCat.find('.count').text(cat.count);
-	});
-
-	$newCat.show();
+	$(this).click(function(){
+		cat.toggle();
+		if(cat.displayStatus){
+			$(this).addClass('active');
+		}else{
+			$(this).removeClass('active');
+		}
+	})
 })
